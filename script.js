c22bf6f2ae06f24f56f888bb699bb05d61271cf9
jonnycs <jberry4@bearcubs.santarosa.edu>
2026-01-13 06:40:33 +0500

    Add displayBooks() function

--- script.js
+++ script.js
@@ -15,11 +15,34 @@ function Book(title, author, totalPages, read) {
 // Creates a book object using the given parameters then pushes it to the end of the myLibrary array
 function addBookToLibrary(title, author, totalPages, read) {
   let book = new Book(title, author, totalPages, read);
-  book.id = crypto.randomUUID();
+  book.id = crypto.randomUUID();  // Give book random unique ID
   myLibrary.push(book);
 }
 
+// Example books
 addBookToLibrary('this is a book', 'lewis', 200, true);
 addBookToLibrary('also a book', 'clark', 15, false);
 
-console.log(myLibrary);
+// Create elements to display book object information and append them to the DOM
+function displayBooks() {
+  for (let libraryIndex = 0; libraryIndex < myLibrary.length; libraryIndex++) {
+    // Create card elements
+    let bookCard = document.createElement('div');
+    let bookName = document.createElement('p');
+    let bookAuthor = document.createElement('p');
+    let bookPage = document.createElement('p');
+    let read = document.createElement('p');
+
+    const cardElements = [bookName, bookAuthor, bookPage, read];
+    const propertyNames = ['title', 'author', 'totalPages', 'read']
+    document.body.appendChild(bookCard);
+
+    // Add card elements inner HTML and append the elements to bookCard
+    for (let elemIndex = 0; elemIndex < cardElements.length; elemIndex++) {
+      cardElements[elemIndex].innerHTML = myLibrary[libraryIndex][propertyNames[elemIndex]]
+      bookCard.appendChild(cardElements[elemIndex]);
+    }
+  }
+}
+
+displayBooks();
